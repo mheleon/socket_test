f5ddf6412dec100c03c742766d6c1b4a9b9e94b3
maykolhdezleon <hmaykol@gmail.com>
2018-08-28 16:30:02 +0200

    Add sent items

--- socket.io-client/index.js
+++ socket.io-client/index.js
@@ -2,28 +2,39 @@
 const io = require('socket.io-client');
 const socket = io.connect('http://localhost:3000', { reconnect: true });
 
-let items = 'ABC';
+ITEMS_REQUESTED = process.env.ITEMS_REQUESTED;
 
 // Add a connect listener
 socket.on('connect', (socketObj) => {
   console.log('Connected');
   // Send items request
-  socket.emit('itemsData', 'ABC');
+  socket.emit('itemsData', ITEMS_REQUESTED);
 });
 
 // Add a itemsData listener
-socket.on('itemsData', (data) =>{
+socket.on('itemsData', (data) => {
   console.log(data);
 });
 
 // Add a broadcast listener
-socket.on('broadcast', (data) =>{
+socket.on('broadcast', (data) => {
   console.log(data);
 });
 
 // Add a notifications listener
-socket.on('notification', (data) =>{
+socket.on('notification', (data) => {
   console.log(data);
 });
 
-// socket.on('disconnect', function(){});
+// Change items requested (simulated)
+let trigger = true;
+setInterval(() => {
+  if (trigger) {
+    socket.emit('itemsData', ["D", "E"]);
+    trigger = false;
+  } else {
+    socket.emit('itemsData', ["A", "B", "C"]);
+    trigger = true;
+  }
+}, 11000);
+
--- socket.io-server/index.js
+++ socket.io-server/index.js
@@ -3,22 +3,26 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const cache = require('./cache.json');
+
+const DELAY = 5000;
+
 let i = 0;
 
 const clients = {};
 
 io.on('connect', (socket) => {
-  console.log('Client connected:', socket.id);
+  console.log('New client connected in socket', socket.id);
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id);
+    console.log('Client disconnected from socket', socket.id);
     console.log(io.sockets.adapter.rooms);
     delete clients[socket.id];
   });
 
   // Configuration event: send items
   socket.on('itemsData', (items) => {
-    console.log('Client', socket.id, 'request items:', items);
+    console.log('Client', socket.id, 'requests items:', items);
     socket.items = items;
     clients[socket.id] = socket;
   });
@@ -27,11 +31,15 @@ io.on('connect', (socket) => {
 // Event: send items
 setInterval(() => {
   for (socketKey in clients) {
-    const d = new Date();
-    clients[socketKey].emit('itemsData', { for: clients[socketKey].id, seg: d.getSeconds(), 'msg#': i, items: clients[socketKey].items });
+    const socket = clients[socketKey];
+    let data = {};
+    for (key in socket.items) {
+      data[socket.items[key]] = (cache[socket.items[key]]);
+    }
+    socket.emit('itemsData', { for: socket.id, 'msg#': i, itemsData: data });
   };
   i++;
-}, 5000);
+}, DELAY);
 
 // Event: broadcast
 setInterval(() => {
